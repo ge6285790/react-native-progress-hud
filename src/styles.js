2d02efadc7500f6d953b6f9429746cc269d56c25
Kyle Cheng <ge6285790@hotmail.com>
2018-03-13 12:11:55 +0800

    adjust color

--- src/styles.js
+++ src/styles.js
@@ -21,7 +21,7 @@ var styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 16,
-    backgroundColor: '#FFFFFF'
+    backgroundColor: 'rgba(0,0,0,.5)'
   },
   spinner: {
     alignItems: 'center',
